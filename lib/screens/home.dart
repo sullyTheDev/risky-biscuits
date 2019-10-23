e827112a37610d71b022f752e5e4f1f9c3ee50d8
Chris Walsh <cwalsh@reliaslearning.com>
2019-10-23 09:36:41 -0400

    adding leaderboard page

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,12 +1,10 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:first_app/screens/leaderboard.dart';
 import 'package:first_app/screens/scores.dart';
 import 'package:flutter/material.dart';
 
 class Home extends StatefulWidget {
-  Home({
-    Key key,
-    this.user
-  }) : super(key: key);
+  Home({Key key, this.user}) : super(key: key);
   final FirebaseUser user;
 
   @override
@@ -28,27 +26,36 @@ class _HomeState extends State<Home> {
       case 0:
         return ScoresPage();
         break;
+      case 1:
+        return LeaderboardPage();
+        break;
       default:
         return ScoresPage();
         break;
     }
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      bottomNavigationBar: BottomNavigationBar(items: const <BottomNavigationBarItem>[
-        BottomNavigationBarItem(icon: Icon(Icons.home), title: Text('Home')),
-        BottomNavigationBarItem(icon: Icon(Icons.hot_tub), title: Text('Hot Tub?')),
-        BottomNavigationBarItem(icon: Icon(Icons.store), title: Text('Store')),
-        BottomNavigationBarItem(icon: Icon(Icons.local_play), title: Text('Tickets')),
-      ],
-      backgroundColor: Colors.white,
-      selectedItemColor: Theme.of(context).primaryColor,
-      unselectedItemColor: Colors.black,
-      unselectedLabelStyle: TextStyle(color: Colors.black),
-      type: BottomNavigationBarType.fixed,
-      onTap: _onItemTapped,
-      currentIndex: _selectedIndex,),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(icon: Icon(Icons.home), title: Text('Home')),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.hot_tub), title: Text('Leaderboard')),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.store), title: Text('Store')),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.local_play), title: Text('Tickets')),
+        ],
+        backgroundColor: Colors.white,
+        selectedItemColor: Theme.of(context).primaryColor,
+        unselectedItemColor: Colors.black,
+        unselectedLabelStyle: TextStyle(color: Colors.black),
+        type: BottomNavigationBarType.fixed,
+        onTap: _onItemTapped,
+        currentIndex: _selectedIndex,
+      ),
       appBar: AppBar(
         title: Text('Home'),
         actions: <Widget>[Icon(Icons.perm_identity)],
@@ -56,5 +63,4 @@ class _HomeState extends State<Home> {
       body: _showTab(_selectedIndex),
     );
   }
-
 }
