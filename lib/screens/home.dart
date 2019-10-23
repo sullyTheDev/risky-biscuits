fcad1fb88b603a580d36f9010f4cc9d96f4524a4
Douglas Coble <dcoble@reliaslearning.com>
2019-10-23 10:43:29 -0400

    fix icon colors

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -32,31 +32,62 @@ class _HomeState extends State<Home> {
         break;
       case 2:
         return LeaderboardPage();
-        break;  
+        break;
       default:
         return ScoresPage();
         break;
     }
   }
 
-  final List<BottomNavigationBarItem> bottomNavItems = [BottomNavigationBarItem(icon: SvgPicture.asset('assets/scoreboard.svg', width: 20.0, height: 20.0,), title: Text('Scores')),
-        BottomNavigationBarItem(icon: SvgPicture.asset('assets/puck.svg', width: 20.0, height: 20.0,), title: Text('Matches')),
-        BottomNavigationBarItem(icon: SvgPicture.asset('assets/leaderboard.svg', width: 20.0, height: 20.0,), title: Text('Leaderboards')),
-        BottomNavigationBarItem(icon: SvgPicture.asset('assets/team.svg', width: 20.0, height: 20.0,), title: Text('Teams'))];
-
   @override
   Widget build(BuildContext context) {
+    final List<BottomNavigationBarItem> bottomNavItems = [
+    BottomNavigationBarItem(
+        icon: SvgPicture.asset(
+          'assets/scoreboard.svg',
+          width: 20.0,
+          height: 20.0,
+          color: _selectedIndex == 0 ? Colors.orange : Colors.black,
+        ),
+        title: Text('Scores')),
+    BottomNavigationBarItem(
+        icon: SvgPicture.asset(
+          'assets/puck.svg',
+          width: 20.0,
+          height: 20.0,
+          color: _selectedIndex == 1 ? Colors.orange : Colors.black,
+        ),
+        title: Text('Matches')),
+    BottomNavigationBarItem(
+        icon: SvgPicture.asset(
+          'assets/leaderboard.svg',
+          width: 20.0,
+          height: 20.0,
+          color: _selectedIndex == 2 ? Colors.orange : Colors.black,
+        ),
+        title: Text('Leaderboards')),
+    BottomNavigationBarItem(
+        icon: SvgPicture.asset(
+          'assets/team.svg',
+          width: 20.0,
+          height: 20.0,
+          color: _selectedIndex == 3 ? Colors.orange : Colors.black,
+        ),
+        title: Text('Teams'))
+  ];
     return Scaffold(
-      bottomNavigationBar: BottomNavigationBar(items: bottomNavItems,
-      backgroundColor: Colors.white,
-      selectedItemColor: Theme.of(context).primaryColor,
-      unselectedItemColor: Colors.black,
-      unselectedLabelStyle: TextStyle(color: Colors.black),
-      type: BottomNavigationBarType.fixed,
-      onTap: _onItemTapped,
-      currentIndex: _selectedIndex,),
+      bottomNavigationBar: BottomNavigationBar(
+        items: bottomNavItems,
+        backgroundColor: Colors.white,
+        selectedItemColor: Theme.of(context).primaryColor,
+        unselectedItemColor: Colors.black,
+        unselectedLabelStyle: TextStyle(color: Colors.black),
+        type: BottomNavigationBarType.fixed,
+        onTap: _onItemTapped,
+        currentIndex: _selectedIndex,
+      ),
       appBar: AppBar(
-        title: this.bottomNavItems[_selectedIndex].title,
+        title: bottomNavItems[_selectedIndex].title,
         actions: <Widget>[Icon(Icons.perm_identity)],
       ),
       body: _showTab(_selectedIndex),
